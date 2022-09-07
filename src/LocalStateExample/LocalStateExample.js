262f38ca1ad52fcca27f93a8d8a24a9790308db0
David <learnitmyway@users.noreply.github.com>
2022-09-07 13:57:07 +0200

    add status to local state example

--- src/LocalStateExample/LocalStateExample.js
+++ src/LocalStateExample/LocalStateExample.js
@@ -1,13 +1,17 @@
 import { useEffect, useState } from "react";
 
-export function LocalState() {
+function People() {
   const [people, setPeople] = useState([]);
+  const [status, setStatus] = useState("idle");
 
   useEffect(() => {
     async function getPeople() {
+      setStatus("loading");
       const response = await fetch("https://www.swapi.tech/api/people");
       if (!response.ok) {
         throw new Error("Network response was not ok");
+      } else {
+        setStatus("success");
       }
       const json = await response.json();
       setPeople(json.results.slice(0, 2));
@@ -15,12 +19,24 @@ export function LocalState() {
     getPeople();
   }, []);
 
+  if (status === "loading") {
+    return <p>Loading...</p>;
+  }
+
   return (
     <>
-      <h3>---------- Local State Example ----------</h3>
       {people.map((person) => (
         <p key={person?.uid}>{person?.name}</p>
       ))}
+    </>
+  );
+}
+
+export function LocalState() {
+  return (
+    <>
+      <h3>---------- Local State Example ----------</h3>
+      <People />
     </>
   );
 }
