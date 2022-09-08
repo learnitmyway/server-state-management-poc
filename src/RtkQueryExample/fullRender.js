eaabef2998e64c4704251d80c1e6124488269516
David <learnitmyway@users.noreply.github.com>
2022-09-08 10:55:34 +0200

    attempt at configuring test store for RTK

--- src/RtkQueryExample/fullRender.js
+++ src/RtkQueryExample/fullRender.js
@@ -4,9 +4,15 @@ import { render } from "@testing-library/react";
 import { rootReducer } from "./store";
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
+import { swapiTechApi } from "./swapiTechApi";
 
 export function configureTestStore({ preloadedState }) {
-  return configureStore({ reducer: rootReducer, preloadedState });
+  return configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(swapiTechApi.middleware),
+    preloadedState,
+  });
 }
 
 const fullRender = (
